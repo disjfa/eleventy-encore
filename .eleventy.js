1ea8862efaad5ee76d473bd4dd3d7f9b26df98dc
disjfa <disjfa@disjfa.nl>
2019-07-09 19:54:16 +0000

    Updated navbar items

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPassthroughCopy("build");
 
+  eleventyConfig.addCollection("nav", function(collection) {
+    return collection.getFilteredByTag("menu").sort((a, b) => a.data.order - b.data.order);
+  });
+
   return {
     pathPrefix: "/eleventy-encore",
 
