8065efb490aa1dea77ee441d341cc7e5873e4cd1
disjfa <disjfa@disjfa.nl>
2020-03-23 20:49:06 +0000

    Updates

--- build/app.40b224b0.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["app"],{dHOs:function(o,n,s){"use strict";s.r(n);s("gRZq");console.log("hello world")},gRZq:function(o,n,s){}},[["dHOs","runtime"]]]);
